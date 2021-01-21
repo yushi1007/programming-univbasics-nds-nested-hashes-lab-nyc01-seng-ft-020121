0d004c7ec357a9e82a2e9abd73a2a87d08882816
Yu Shi <yushiys95@gmail.com>
2021-01-21 20:01:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,7 @@ def literal_aoh
    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
    { name:  "Pat Monteseian", occupation: "Staff" },
    { name:  "Kate Gompert", occupation: "None" },
-   { name:  "Bruce Green", occupation: "Fan of Mildred" }
+   { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
